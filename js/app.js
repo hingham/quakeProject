fad0115669c1fef10054b3818e159253a5dd308e
hingham <hannah.c.ingham@gmail.com>
2018-10-01 12:47:33 -0700

    restored map

--- js/app.js
+++ js/app.js
@@ -1,6 +1,5 @@
 'use strict'
 
-var earthquakeArray = [];
    
       var map;
       function initMap() {
@@ -16,28 +15,52 @@ var earthquakeArray = [];
         // http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojsonp
         script.src = 'https://developers.google.com/maps/documentation/javascript/examples/json/earthquake_GeoJSONP.js';
         document.getElementsByTagName('head')[0].appendChild(script);
+
+        console.log(map);
+
       }
 
       
-  
-      window.eqfeed_callback = function(results) { //what does eqfeed_callback mean?
+  //sorts by magnitude 
+      // window.eqfeed_callback = function(results) { 
+      //   for (var i = 0; i < 10; i++) {
+      //       console.log(typeof(results));
+      //       console.log(typeof(results.features));
+      //       console.log(typeof(results.features[i].properties.mag));
+      //       if(results.features[i].properties.mag > 5){
+      //           var coords = results.features[i].geometry.coordinates;
+      //           var latLng = new google.maps.LatLng(coords[1],coords[0]);
+      //           var marker = new google.maps.Marker({
+      //               position: latLng,
+      //               map: map,
+      //           });
+      //       }
+      //     console.log(results);
+      //     //localStorage.setItem("earthquakeKey", JSON.stringify(results));
+      //   }
+      // }
+
+//sorts throught the string
+      window.eqfeed_callback = function(results) { 
         for (var i = 0; i < 10; i++) {
-            console.log(typeof(results));
-            console.log(typeof(results.features));
-            console.log(typeof(results.features[i].properties.mag));
-            if(results.features[i].properties.mag > 5){
+            console.log(typeof(results.features[i].properties.place));
+            var strInput=results.features[i].properties.place;
+            if(strInput.includes('Philippines')){
                 var coords = results.features[i].geometry.coordinates;
                 var latLng = new google.maps.LatLng(coords[1],coords[0]);
                 var marker = new google.maps.Marker({
                     position: latLng,
                     map: map,
-                    
                 });
             }
           console.log(results);
           //localStorage.setItem("earthquakeKey", JSON.stringify(results));
         }
       }
+
+
+
+
 
 
 
