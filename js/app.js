4ed28e5b43f76763fa0736e338f1adc5778b6f08
BehelitFam <mriodine@gmail.com>
2018-10-03 13:48:53 -0700

    added geocoder. added address search functionality. added new search form that allows users to choose between address and lat/lng search functionality. Refactored some map initialization/reloading functions for easier reading. added rudimentary form reset. Ugly code but it all works.

--- js/app.js
+++ js/app.js
@@ -8,15 +8,15 @@ function addElement(element, content, parent) {
   return newElement;
 }
 
-
-var body =document.getElementById('body');
+var body = document.getElementById('body');
 var triggerShake = document.getElementById('shake');
 var quakeInfo = document.getElementById('quakeData');
 var quakes = localStorage.getItem('mapQuakes');
 var earthquakeInfo = JSON.parse(quakes);
 
-
 var map;
+var geocoder;
+var gMarkers = [];
 
 var magRange = [0, 40];
 var searchLocation;
@@ -25,53 +25,89 @@ var searchRadius;
 var filterForm = document.getElementById('filterForm');
 filterForm.addEventListener('submit', changeFilters);
 
+
+function locTypeCheck() {
+  var filtLat = document.getElementById('filterLatitude');
+  var filtLng = document.getElementById('filterLongitude');
+  var filtAddr = document.getElementById('filterAddress');
+  var filtRad = document.getElementById('filterRadius');
+  if (document.getElementById("selectAddress").checked) {
+    filtLat.style.display = "none";
+    filtLng.style.display = "none";
+    filtAddr.style.display="block";
+    filtRad.style.display = "block";
+    filtLat.value = filtLat.defaultValue;
+    filtLng.value = filtLng.defaultValue;
+  } else if (document.getElementById("selectLatLng").checked) {
+    filtLat.style.display = "block";
+    filtLng.style.display = "block";
+    filtAddr.style.display="none";
+    filtRad.style.display = "block";
+    filtAddr.value = filtAddr.defaultValue;
+  }
+}
+
 function changeFilters() {
   event.preventDefault();
-  var magInput = document.getElementById('filterMagnitude');
-  var longInput = document.getElementById('filterLongitude');
-  var latInput = document.getElementById('filterLatitude');
-  var radInput = document.getElementById('filterRadius');
+  var magInput = document.getElementById('filterMagnitude').value;
+  var longInput = document.getElementById('filterLongitude').value;
+  var latInput = document.getElementById('filterLatitude').value;
+  var addressInput = document.getElementById('filterAddress').value;
+  var radInput = document.getElementById('filterRadius').value;
 
-  if (magInput.value) {
-    magRange[0] = parseInt(magInput.value, 10);
+  if (magInput) {
+    magRange[0] = parseInt(magInput, 10);
   }
 
-  if (longInput.value && latInput.value && radInput.value) {
-    searchLocation = new google.maps.LatLng(latInput.value, longInput.value);
-    searchRadius = radInput.value;
-  } else if (longInput.value || latInput.value || radInput.value ) {
-    console.log('bad input');
+  if (longInput && latInput && radInput) {
+    var loc = new google.maps.LatLng(latInput, longInput);
+    console.log(loc);
+    searchLocation = loc;
+    map.setCenter(loc);
+    searchRadius = radInput;
+    reloadMap();
+  } else if (addressInput && radInput) {
+    var loc;
+    var test;
+    geocoder.geocode({'address': addressInput}, function(results, status) {
+      if (status == 'OK') {
+        loc = results[0].geometry.location;
+        test = 5;
+        searchLocation = loc;
+        map.setCenter(loc);
+        searchRadius = radInput;
+        reloadMap();
+        return;
+      } else {
+        console.log('Geocoder couldn\'t find entered address');
+      }
+    });
+  } else {
+    reloadMap();
   }
   
-  console.log(magRange);
-  initMap();
-  loadQuakes(JSON.parse(localStorage.getItem('mapQuakes')));
 }
 
-window.eqfeed_callback = function(results) { 
-  console.log('entered eqfeed_callback');
-  if (!localStorage.getItem('mapQuakes')) {
-    console.log('no stored data');
-    localStorage.setItem('mapQuakes', JSON.stringify(results));
-  }
-  
-  initMap();
-  loadQuakes(JSON.parse(localStorage.getItem('mapQuakes')));
-}
+
 
 function initMap() {
   document.getElementById('map').innerHTML = '';
   map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 4,
+    zoom: 3,
     center: new google.maps.LatLng(47.6062095,-122.3320708),
     mapTypeId: 'terrain'
   }); 
+  geocoder = new google.maps.Geocoder();
+  loadQuakes();
 }
 
+function reloadMap() {
+  clearMarkers();
+  loadQuakes();
+}
 
-
-function loadQuakes(results) {
-  var results = results;
+function loadQuakes() {
+  var results = JSON.parse(localStorage.getItem('mapQuakes'));
   console.log("loaded quakes");
   
   for (var i = 0; i < results.features.length; i++) {
@@ -82,6 +118,7 @@ function loadQuakes(results) {
         map: map,
         icon: getIcon(results.features[i].properties.mag)
       });
+      gMarkers.push(marker);
       marker.informationIndex = i;
       marker.addListener('click', function(e) {
         quakeInfo.innerHTML = '';
@@ -97,6 +134,13 @@ function loadQuakes(results) {
 }
 
 
+function clearMarkers() {
+  for (var i=0; i<gMarkers.length; i++) {
+    gMarkers[i].setMap(null);
+  }
+  gMarkers = [];
+}
+
 // Returns true if magnitude filter is not set, or if marker's magnitude falls within 
 function filterMagnitude(quakeMag) {
   var willShow = true;
@@ -104,8 +148,6 @@ function filterMagnitude(quakeMag) {
   if(quakeMag < magRange[0] || quakeMag > magRange[1]) {
     console.log('feature excluded by mag');
     willShow = false;
-  } else {
-    console.log('feature loaded');
   }
   return willShow;
 }
@@ -118,8 +160,6 @@ function filterLocation(position) {
     if (distance > searchRadius) {
       console.log('feature excluded by distance');
       willShow = false;
-    } else {
-      console.log('feature loaded');
     }
   }
   return willShow;
@@ -147,6 +187,15 @@ function handleClick(event){
   body.setAttribute('class', 'shake');
 }
 
+
+window.eqfeed_callback = function(results) { 
+  console.log('entered eqfeed_callback');
+  if (!localStorage.getItem('mapQuakes')) {
+    console.log('no stored data');
+    localStorage.setItem('mapQuakes', JSON.stringify(results));
+  }
+  initMap();
+}
 
 triggerShake.addEventListener('click', handleClick);
 
